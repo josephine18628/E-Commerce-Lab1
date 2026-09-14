d00cdf6c2d8197c84b4552eb476ce72c546b3b47
josephine18628 <josephine.allan@ashesi.edu.gh>
2026-09-14 22:14:33 +0000

    Change database user and password placeholders
    Updated database credentials to use placeholders for user and password.

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
-$db_user = "josephine.allan";
-$db_pass = "";
+$db_user = "USERNAME";
+$db_pass = "YOUR_NEW_PASSWORD";
 $db_name = "ecommerce_2026A_josephine_allan";
 $conn = new mysqli($host, $db_user, $db_pass, $db_name);
 if ($conn->connect_error) {
